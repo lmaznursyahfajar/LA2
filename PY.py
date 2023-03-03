5776254a9f5ef14f010143c28ef9a452d3560bec
lmaznursyahfajar <126775407+lmaznursyahfajar@users.noreply.github.com>
2023-03-03 20:19:38 +0700

    100

--- PY.py
+++ PY.py
@@ -2,9 +2,4 @@ print("hai selamat datang di Github")
 print("hello world")
 a = 3+5
 
-class mobil:
-
-    def __init__(self,merk,harga):
-        self.merk(merk)
-        self.harga(harga)
-        return()
+print('hai')
